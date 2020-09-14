68621cea8187e0e44ad3b505df462f9dbca55ce9
RishuRoy <rishukumar.prince@gmail.com>
2020-09-15 00:30:18 +0530

    Basic Router Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,11 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ContactComponent } from './contact/contact.component';
+
 // import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
@@ -29,7 +34,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -40,6 +48,7 @@ import { FooterComponent } from './footer/footer.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    AppRoutingModule,
     // FontAwesomeModule
   ],
   providers: [
